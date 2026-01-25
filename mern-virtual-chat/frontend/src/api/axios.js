db425687112bd40c4eb0348b1f1570886a0c65fc
Gnana Prakash <134157630+raja1234590@users.noreply.github.com>
2026-01-25 23:13:48 +0530

    Update axios baseURL for environment-specific usage
    Adjust baseURL to use different environment variables for development and production.

--- mern-virtual-chat/frontend/src/api/axios.js
+++ mern-virtual-chat/frontend/src/api/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_URL
+      : process.env.REACT_APP_BACKEND_URL,
+  withCredentials: true,
 });
+
+console.log("AXIOS BASE URL =>", api.defaults.baseURL);
 
 export default api;
